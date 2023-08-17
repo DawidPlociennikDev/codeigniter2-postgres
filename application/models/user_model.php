e14e197c8714601d3740be1910f0fbd5d9fe7053
unknown <nean12.bg@gmail.com>
2023-08-17 17:03:02 +0200

    analysis

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,26 +3,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User_model extends CI_Model
 {
-    public function get_user_by_email(string $email)
+    public function get_user_by_email(string $email) : object
     {
         $this->db->where('email', $email);
         $query = $this->db->get('users');
         return $query->row();
     }
 
-    public function get_user_by_emailEXPLAIN(string $email)
-    {
-        // $query = $this->db->query("EXPLAIN SELECT * FROM users WHERE email = " . $email);
-        // return $query->result_array();
-    }
-
-    public function get_user_by_emailSTATS(string $email)
-    {
-        $query = $this->db->query("SELECT * FROM pg_stat_statements");
-        return $query->result_array();
-    }
-
-    public function insert_user($data)
+    public function insert_user($data) : int
     {
         $this->db->insert('users', $data);
         return $this->db->insert_id();
