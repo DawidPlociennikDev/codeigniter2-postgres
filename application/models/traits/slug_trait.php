1f59534131f78fbc85af910a55c2603c02daa1a7
unknown <nean12.bg@gmail.com>
2023-08-20 11:01:24 +0200

    trait for slug

---
+++ application/models/traits/slug_trait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Models\Traits;
+
+trait SlugTrait
+{
+    public function slugName(string $stringToSlug): string
+    {
+        return url_title($stringToSlug, 'dash', TRUE);
+    }
+}
